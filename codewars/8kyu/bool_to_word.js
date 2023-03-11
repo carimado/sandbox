a4c3c0f08efb7e84e5df0bcad251a8cbb71c5cf9
clarence <clarencejames@live.com.au>
2023-03-12 02:08:49 +1100

    bool_to_word

--- codewars/8kyu/bool_to_word.js
+++ codewars/8kyu/bool_to_word.js
@@ -1,20 +1,13 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
-// function boolToWord(bool) {
-//     if (typeof bool === 'string') {
-//         return true
-//     }
-//     return false
-// }
-
-// // TESTS
-// console.log(boolToWord('Clarence'))
-// console.log(boolToWord(3))
-// console.log(boolToWord(['Array']))
-
-function boolToWord(bool){
-    if (bool === true) {
-        return 'Yes'
+function boolToWord(bool) {
+    if (typeof bool === 'string') {
+        return true
     }
-    return 'No'
+    return false
 }
+
+// TESTS
+console.log(boolToWord('Clarence'))
+console.log(boolToWord(3))
+console.log(boolToWord(['Array']))
