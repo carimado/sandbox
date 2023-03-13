80897140769e68c7ef8d0d820372b9f4e6c39dfc
clarence <clarencejames@live.com.au>
2023-03-13 20:44:34 +1100

    complementary_dna

--- codewars/7kyu/complementary_dna.js
+++ codewars/7kyu/complementary_dna.js
@@ -4,25 +4,31 @@
 // "ATTGC" --> "TAACG"
 // "GTAT" --> "CATA"
 
-function DNAStrand(dna){
-    let complementarySide = ''
-    for (let i = 0; i < dna.length; i++){
-        char = dna[i]
-        if (char === "A") {
-            complementarySide += "T"
-        } else if (char === "G") {
-            complementarySide += "C"
-        } else if (char === "T") {
-            complementarySide += "A"
-        } else if (char === "C") {
-            complementarySide += "G"
-        } else {
-            complementarySide += "C"
-        }
-    }
-    return complementarySide
-}
+// First Attempt - Success
 
+// function DNAStrand(dna){
+//     let complementarySide = ''
+//     for (let i = 0; i < dna.length; i++){
+//         char = dna[i]
+//         if (char === "A") {
+//             complementarySide += "T"
+//         } else if (char === "G") {
+//             complementarySide += "C"
+//         } else if (char === "T") {
+//             complementarySide += "A"
+//         } else if (char === "C") {
+//             complementarySide += "G"
+//         } else {
+//             complementarySide += "C"
+//         }
+//     }
+//     return complementarySide
+// }
+
+// Clever Solution from another user:
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+const DNAStrand = dna => dna.replace(/./g, c => pairs[c]);
 
 console.log(DNAStrand("ATTGC"))
 console.log(DNAStrand("GTAT"))
+
