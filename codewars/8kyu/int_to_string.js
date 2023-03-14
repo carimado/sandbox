fab6f0f3eee72de32b574050e803a577b99a2146
clarence <clarencejames@live.com.au>
2023-03-14 21:30:51 +1100

    int_to_string

---
+++ codewars/8kyu/int_to_string.js
@@ -0,0 +1,17 @@
+// We need a function that can transform a number (integer) into a string.
+
+// Solution 1
+function numberToString(num) {
+    return num.toString()
+}
+
+// Solution 2
+// console.log(String(52))
+
+// Solution 3 
+// function numberToString(num) {
+//     return ""+num
+// }
+
+console.log(numberToString(67))
+console.log(numberToString(202))
